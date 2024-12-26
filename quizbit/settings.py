086a3e6ff888eb895b744422e6b9fe1fcb289b91
YeakubSadli <yakubsadlil@gmail.com>
2024-12-26 20:38:08 +0600

    add settings in .env

--- quizbit/settings.py
+++ quizbit/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-9&n&@gy%i%$l317-$fav3+x)f2(0(x46sr!m#1xy34h8n^0x!z'
+SECRET_KEY = os.getenv("DJANGO_Scrt_KEY")
 
 DEBUG = True
 
@@ -127,6 +127,6 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'seyamsadlil647@gmail.com'
-EMAIL_HOST_PASSWORD = 'impk bipt niax ldfh'
+EMAIL_PORT = os.getenv("E_PORT")
+EMAIL_HOST_USER = os.getenv("E_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("E_HOST_PASSWORD")
